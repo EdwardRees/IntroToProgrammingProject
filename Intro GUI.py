1ebd608230e24d8276142e99bb750503a5a6debd
rayteo <teoyongrui@gmail.com>
2016-05-13 09:26:02 +0800

    Create Intro GUI.py

---
+++ Intro GUI.py
@@ -0,0 +1,11 @@
+from Tkinter import *
+win = Tk()
+
+win.title('Balance Board')
+win.geometry('1440x990') # Size 200, 200
+
+btn = Button(win, text='Start')
+btn.pack(expand=YES, fill=BOTH)
+
+
+mainloop()
