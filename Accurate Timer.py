b652539e0cc26c550dd126bebd58de6184850700
180998 <180998@Hkis.edu.hk>
2016-05-23 10:54:45 +0800

    Update Accurate Timer.py

--- Accurate Timer.py
+++ Accurate Timer.py
@@ -1,17 +1,17 @@
-import time 
-  
+import time
+
 Sec=0
 Min=0
 Lap=0
 
-time1=time.time()
-time2=time.time()
+Start=time.time()
+End=time.time()
 
 while True:
 
   time.sleep(1)
-  time2=time.time()
-  print(time2-time1)
+  End=time.time()
+  print(End-Start)
   Sec += 1
   secClock = (str(Min) + " Mins " + str(Sec) + " Sec ")
   print secClock
